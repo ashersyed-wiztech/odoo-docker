cd98cf76d6cb290e3e599f5d00a816a782b78a88
Asher Syed <asher.syed@novigi.com.au>
2019-04-10 17:05:54 +0500

    modifying some values

--- 12.0/entrypoint.sh
+++ 12.0/entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 
 # set the postgres database host, port, user and password according to the environment
 # and pass them as arguments to the odoo process if not present in the config file
-: ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
-: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
-: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+# : ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
+# : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+# : ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+# : ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
 
 DB_ARGS=()
 function check_config() {
@@ -18,10 +18,10 @@ function check_config() {
         DB_ARGS+=("${value}")
    fi;
 }
-check_config "db_host" "$HOST"
-check_config "db_port" "$PORT"
-check_config "db_user" "$USER"
-check_config "db_password" "$PASSWORD"
+check_config "db_host" "$POSTGRESQL_HOST"
+check_config "db_port" "$POSTGRESQL_PORT_NUMBER"
+check_config "db_user" "postgres"
+check_config "db_password" "$POSTGRESQL_PASSWORD"
 
 case "$1" in
     -- | odoo)

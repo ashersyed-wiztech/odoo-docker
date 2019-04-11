af6c4a465dd48b1b97df0c61e0bd9804351bd11a
Asher Syed <asher.syed@novigi.com.au>
2019-04-11 18:06:59 +0500

    fixing deploy script

--- publish-odoo-image.sh
+++ publish-odoo-image.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-curl header "PRIVATETOKEN: RRB5Wesw99zMd3sjpxm" 'http://172.19.53.32/api/v4/projects' > projects.json
-curl header "PRIVATETOKEN: RRB5Wesw99zMd3sjpxm" 'http://172.19.53.32/api/v4/projects/1/jobs?scope[]=success' > jobs.json
+set -x
+
+curl --header "PRIVATETOKEN: RRB5Wesw99zMd3sjpxm" 'http://172.19.53.32/api/v4/projects' -o projects.json
+curl --header "PRIVATETOKEN: RRB5Wesw99zMd3sjpxm" 'http://172.19.53.32/api/v4/projects/1/jobs?scope[]=success' -o jobs.json
 
 ARTIFACTS_URL=$(cat jobs.json |  jq '.[0].web_url')
 ARTIFACTS_URL=${ARTIFACTS_URL//\"/}
@@ -14,3 +16,5 @@ docker login --username=kevin_huan@kcre.com.cn --password k53085771 registry.cn-
 docker push registry.cn-shanghai.aliyuncs.com/silu-design/odoo:12
 
 popd
+
+set +x

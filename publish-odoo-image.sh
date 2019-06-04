dba632342447bc187ea05978738b5a5687f22d98
Silu Dev <dev@silu.design>
2019-06-05 01:30:38 +0800

    Updated Artifact URL

--- publish-odoo-image.sh
+++ publish-odoo-image.sh
@@ -5,10 +5,10 @@ set -x
 curl --header "PRIVATE-TOKEN: RRB-5Wesw99zMd3sjpxm" 'http://172.19.53.32/api/v4/projects' -o projects.json
 curl --header "PRIVATE-TOKEN: RRB-5Wesw99zMd3sjpxm" 'http://172.19.53.32/api/v4/projects/1/jobs?scope[]=success' -o jobs.json
 
-ARTIFACTS_URL=$(cat jobs.json |  jq '.[1].web_url')
-ARTIFACTS_URL=${ARTIFACTS_URL//\"/}
-ARTIFACTS_URL=${ARTIFACTS_URL}/artifacts/download
-
+#ARTIFACTS_URL=$(cat jobs.json |  jq '.[1].web_url')
+#ARTIFACTS_URL=${ARTIFACTS_URL//\"/}
+#ARTIFACTS_URL=${ARTIFACTS_URL}/artifacts/download
+ARTIFACTS_URL="http://172.19.53.32/api/v4/projects/1/jobs/artifacts/12.0/download?job=job"
 pushd 12.0/
 
 docker build --build-arg ARTIFACTS_URL="$ARTIFACTS_URL" -t registry.cn-shanghai.aliyuncs.com/silu-design/odoo:12 .

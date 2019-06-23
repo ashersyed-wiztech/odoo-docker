f194b136a4e423c3035cbe14d542fd31f327cddc
matt <matt@onmogu.com>
2019-06-23 16:21:54 +0800

    Update publish-odoo-image.sh - added no cache

--- publish-odoo-image.sh
+++ publish-odoo-image.sh
@@ -11,7 +11,7 @@ curl --header "PRIVATE-TOKEN: RRB-5Wesw99zMd3sjpxm" 'http://172.19.53.32/api/v4/
 ARTIFACTS_URL="http://172.19.53.32/api/v4/projects/1/jobs/artifacts/12.0/download?job=job"
 pushd 12.0/
 
-docker build --build-arg ARTIFACTS_URL="$ARTIFACTS_URL" -t registry.cn-shanghai.aliyuncs.com/silu-design/odoo:12 .
+docker build --no-cache --build-arg ARTIFACTS_URL="$ARTIFACTS_URL" -t registry.cn-shanghai.aliyuncs.com/silu-design/odoo:12 .
 echo $REGISTRYPASS | docker login --username=$REGISTRYUSER --password-stdin registry.cn-shanghai.aliyuncs.com
 docker push registry.cn-shanghai.aliyuncs.com/silu-design/odoo:12
 

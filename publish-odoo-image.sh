fcd540d16d1507bbeac54fd54076c6d315b5a803
Asher Syed <asher.syed@wiztech.pk>
2019-10-30 17:56:52 +0500

    changing base image to debian:buster, installing python os packages required for odoo and install odoo with install recommends

--- publish-odoo-image.sh
+++ publish-odoo-image.sh
@@ -15,8 +15,13 @@ ARTIFACTS_URL="http://47.102.156.191/api/v4/projects/1/jobs/${JOB_ID}/artifacts"
 pushd 12.0/
 
 docker build --build-arg ARTIFACTS_URL="$ARTIFACTS_URL" -t registry.cn-shanghai.aliyuncs.com/silu-design/odoo:12 .
+# docker tag registry.cn-shanghai.aliyuncs.com/silu-design/odoo:12 registry-vpc.cn-shanghai.aliyuncs.com/silu-design/odoo:12
+
 echo $REGISTRYPASS | docker login --username=$REGISTRYUSER --password-stdin registry.cn-shanghai.aliyuncs.com
+# echo $REGISTRYPASS | docker login --username=$REGISTRYUSER --password-stdin registry-vpc.cn-shanghai.aliyuncs.com
+
 docker push registry.cn-shanghai.aliyuncs.com/silu-design/odoo:12
+# docker push registry-vpc.cn-shanghai.aliyuncs.com/silu-design/odoo:12
 
 popd
 
